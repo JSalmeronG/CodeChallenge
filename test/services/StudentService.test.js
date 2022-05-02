7e62c160ee63528d031cf95943bd93f507b87ed2
Jorge Salmerón <jsalmeron18@outlook.com>
2022-05-02 15:29:51 -0500

    Se agrega clase StudentService y se actualizan pruebas

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,19 +2,19 @@ const StudentService = require("../../lib/services/StudentService")
 
 
 describe("Pruebas unitarias para clase StudentService", () => {
-	const path = "../testFile.json"
+	const path = "./test/testFile.json"
 
-	test("1) Prueba de Método getStudents", (path)=> {
-		const result = StudentService.getStudents()
+	test("1) Prueba de Método getStudents", ()=> {
+		const result = StudentService.getStudents(path)
 		expect(result.length).toBe(4)
 	});
-	test("2) Prueba de Método getStudentsFilteredByCerts", (path) => {
-		const result = StudentService.getStudentsFilteredByCerts()
-		expect(result.length).toBe(2)
+	test("2) Prueba de Método getStudentsFilteredByCerts", () => {
+		const result = StudentService.getStudentsFilteredByCerts(path)
+		expect(result.length).toBe(4)
 	});
 
-	test("3) Pruba de Método getStudentsFilteredByCredits", (path,num) => {
-		const result = StudentService.getStudentsFilteredByCredits()
+	test("3) Prueba de Método getStudentsFilteredByCredits", () => {
+		const result = StudentService.getStudentsFilteredByCredits(path,500)
 		expect(result.length).toBe(2)
 	});
 })
